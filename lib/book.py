85466ac12bc674b3fd560506af8618f9d31eb51b
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-11-14 16:57:00 -0500

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # @property
+    # def title(self):
+    #     return self._title
+
+    # @title.setter
+    # def title(self, new_title):
+    #     self._title = new_title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, new_page):
+        if(isinstance(new_page, int)):
+            self._page_count = new_page
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
